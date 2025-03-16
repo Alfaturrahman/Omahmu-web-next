1fd96ee2af5ddec7009e696239469d4384c460ac
Alfaturrahman <alfatur147@gmail.com>
2025-03-16 16:43:59 +0700

    PUSH BY ALFATUR

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    output: "standalone", // Untuk memastikan bisa dijalankan di Vercel
+    experimental: {
+      appDir: true, // Pastikan Next.js tahu kamu pakai App Router
+    },
+    reactStrictMode: true, // Mode strict untuk debugging lebih baik
+  };
+  
+  export default nextConfig;
+  
