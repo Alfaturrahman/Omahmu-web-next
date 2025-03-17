ae4af711159731264fd140f0b70302f5ac3590eb
Alfaturrahman <alfatur147@gmail.com>
2025-03-17 14:16:25 +0700

    PUSH BY ALFATUR

--- app/page.js
+++ app/page.js
@@ -63,7 +63,7 @@ export default function Home() {
             {/* Footer */}
             <footer id="kontak" className="bg-blue-600 text-white py-6 px-4">
                 <div className="container mx-auto text-center">
-                    <p>&copy; 2025 Landing Page. All rights reserved.</p>
+                    <p>&copy; 2025 Landing Page. All rights reserved .</p>
                 </div>
             </footer>
         </div>
